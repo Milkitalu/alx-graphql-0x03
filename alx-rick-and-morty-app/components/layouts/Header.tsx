90ff9f07d06f8ca5f0abea561f0fd513579ab3fc
Talaku Getahun <getahuntalaku@gmail.com>
2025-02-02 20:47:45 +0300

    For Create the ErrorBoundary Component

---
+++ alx-rick-and-morty-app/components/layouts/Header.tsx
@@ -0,0 +1,39 @@
+ 
+import Link from "next/link"; 
+import Button from "../common/Button"; 
+import { usePathname } from "next/navigation"; 
+import { useCount } from "@/context/CountContext";
+
+const Header: React.FC = () => {
+
+const pathname = usePathname(); 
+const { count } = useCount();
+
+return (
+
+<header>
+    <div className="flex gap-4">
+      {
+        !["/counter-app"].includes(pathname) ? (
+        <>
+            <Button
+                buttonLabel="Sign In"
+                buttonBackgroundColor="red"
+            />
+
+            <Button
+              buttonLabel="Sign Up"
+              buttonBackgroundColor="blue"
+            />
+      </>
+        ) : (
+          <p className=" font-semibold text-lg">Current count : {count}</p>
+        )
+      }
+
+    </div>
+</header>
+); 
+};
+
+export default Header; 
